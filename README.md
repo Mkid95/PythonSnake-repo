8d33849d14e7e0048e4ffd49debb6367506e080f
Mkid95 <matthewbespinoza@gmail.com>
2021-10-12 14:49:25 -0400

    code clean up and polish

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PythonSnake-repo
 The classic game snake made with Python and the Pygame API
+
+This was a fun project to work on. I initially had problems getting movement working 
+the way I would like to which was tied to the refresh rate of my monitor but manage to
+change it to a time based event.
--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import random
 import pygame
-
+"""
+Basic snake game made using pygame
+"""
 pygame.init()
-length = 5
-RIGHT, DOWN, LEFT, UP = 0, 1, 2, 3
-
-MOVE = 50
+length = 5 #initial length of the snake
+RIGHT, DOWN, LEFT, UP = 0, 1, 2, 3 #constants to define directions initial direction is RIGHT
+MOVE = 50 # the rate at which movement is calculated in ms
 speed = 20
 direction = RIGHT
 move_event = pygame.USEREVENT + 1
 screen = pygame.display.set_mode((840, 600))
 pygame. display.set_caption("Snake")
-
+clock = pygame.time
 snakeImg = pygame.image.load('snek.png')
 appleImg = pygame.image.load('python.png')
 picture = pygame.transform.scale(snakeImg, (20, 20))
 apple = pygame.transform.scale(appleImg, (20, 20))
-snakeX = 0
-snakeY = 0
-foodX = 0
-foodY = 0
-trail = []
+snakeX, snakeY, foodX, foodY = 0, 0, 0, 0
+trail = [] #array of snake's trail or body. Only stores coordinates
 
 
 def spawn_snake():
@@ -53,12 +51,13 @@ def spawn_food():
 
 
 def die():
-    largeText = pygame.font.SysFont("comicsansms", 100)
+    largeText = pygame.font.SysFont("arial", 100)
     TextSurf = largeText.render('Game Over', False, (0, 0, 0))
     draw_snake()
     screen.blit(apple, (foodX, foodY))
     screen.blit(TextSurf, (170, 200))
     pygame.display.update()
+    clock.wait(1000)
     paused = True
     while paused:
         for event in pygame.event.get():
@@ -69,11 +68,12 @@ def die():
     spawn_food()
     spawn_snake()
 
+#Core game loop initiates here
+
 
 spawn_food()
 pygame.time.set_timer(move_event, MOVE)
 change_direction = False
-'#loop'
 running = True
 while running:
     screen.fill((128, 128, 128))
@@ -94,7 +94,7 @@ while running:
             if event.key == pygame.K_DOWN and direction != UP:
                 direction = DOWN
                 change_direction = True
-        if event.type == move_event:
+        if event.type == move_event: #movement event moves the snake and checks if the movement kills the player or if the player has eaten the food.
             trail.append((snakeX, snakeY))
             if direction == RIGHT:
                 snakeX += speed
